ce1a5498c115c19041711f7167e76b1653bf74d9
Snaju003 <soumyaraj2003@gmail.com>
2023-11-03 01:05:17 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,64 @@
-import pywhatkit as pw
+import tkinter as tk
+import requests
+from tkinter import messagebox
+from PIL import Image,ImageTk
+import ttkbootstrap
 
-txt="""Python is sexy"""
+def get_weather(city):
+    API_key = "b8ff2e6b546e607c4a5767c61ecb82a1"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}"
+    res = requests.get(url)
 
-pw.text_to_handwriting(txt,"WhatsApp Image 2023-11-02 at 12.22.16 AM.jpeg")
-print(" END ")
+    if res.status_code == 404:
+        messagebox.showerror("Error","City Not Found")
+        return None
+    
+    weather = res.json()
+    icon_id = weather['weather'][0]['icon']
+    temperature = weather['main']['temp'] -273.15
+    description = weather['weather'][0]['description']
+    city = weather['name']
+    country = weather['sys']['country']
+
+    icon_url = f"https://openweathermap.org/img/wn/{icon_id}@2x.png"
+    return (icon_url, temperature, description, city, country)
+
+def search():
+    city = city_entry.get()
+    result = get_weather(city)
+    if result is None:
+        return
+    icon_url, temperature, description, city, country = result
+    location_label.configure(text=f"{city},{country}")
+
+    image = Image.open(requests.get(icon_url,stream=True).raw)
+    icon = ImageTk.PhotoImage(image)
+    icon_label.configure(image=icon)
+    icon_label.image = icon
+
+    temperature_label.configure(text=f"Temperature: {temperature:.2f}°C")
+    description_label.configure(text=f"Description: {description}")
+
+root = ttkbootstrap.Window(themename="morph")
+root.title("Weather App")
+root.geometry("400x400")
+
+city_entry = ttkbootstrap.Entry(root, font="Helventica, 18")
+city_entry.pack(pady=10)
+
+search_button = ttkbootstrap.Button(root, text="Search",command=search, bootstyle="Warning")
+search_button.pack(pady=10)
+
+location_label = tk.Label(root, font="Helvetica, 25")
+location_label.pack(pady=20)
+
+icon_label = tk.Label(root)
+icon_label.pack()
+
+temperature_label = tk.Label(root, font="Helvetica, 20")
+temperature_label.pack()
+
+description_label = tk.Label(root, font="Helvetica, 20")
+description_label.pack()
+
+root.mainloop()
